cfd7767a4f90e7dfdd6cae00aafd071b75a48b5c
Zla <zlatka.mihaylova@googlemail.com>
2013-10-25 22:04:40 +0200

    collision detection script

---
+++ Game/Assets/CollisionDetection.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour
+{
+	
+	Vector3 pos;
+	Vector3 lastPosition;
+	bool collided = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!collided){
+		if (Input.GetKey("space")){
+			transform.Translate(Vector3.left *Time.deltaTime);
+			lastPosition = pos;
+			pos = transform.position;
+		}
+		}
+		
+	}
+
+	void OnCollisionEnter(){
+		collided = true;
+		Debug.Log("here");
+		transform.position = lastPosition;
+		pos = lastPosition;
+	}
+	
+	void OnCollisionExit(){
+		collided = false;
+	}
+	
+	
+	
+}
