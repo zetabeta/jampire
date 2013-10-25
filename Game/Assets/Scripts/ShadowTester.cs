99d20178ae2943ae377fbc94acaf506c3ada189e
THplusplus <thomas.hilbert@gameforge.com>
2013-10-25 22:21:22 +0200

    fdfg

---
+++ Game/Assets/Scripts/ShadowTester.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowTester : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		int lightHits = 0;
+
+		foreach (GameObject lightSource in GameObject.FindGameObjectsWithTag("LightSources"))
+		{
+			if ((transform.position - lightSource.transform.position).sqrMagnitude > (lightSource.light.range * lightSource.light.range))
+				continue;
+
+			if (Physics.Raycast(transform.position, lightSource.transform.position - transform.position, (transform.position - lightSource.transform.position).magnitude, 1 << LayerMask.NameToLayer("ShadowCasters")))
+				continue;
+
+			++lightHits;
+		}
+
+		if (lightHits > 0)
+			GetComponentInChildren<Renderer>().material.color = Color.red;
+		else
+			GetComponentInChildren<Renderer>().material.color = Color.white;
+	}
+}
