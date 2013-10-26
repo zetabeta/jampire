1dc95b33bc7a56d90527205d4ca0b301218e1e44
THplusplus <thomas.hilbert@gameforge.com>
2013-10-26 16:05:19 +0200

    a

--- Game/Assets/Scripts/DragonManager.cs
+++ Game/Assets/Scripts/DragonManager.cs
@@ -10,7 +10,7 @@ public class DragonManager : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
 		{
 			Ray mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -21,28 +21,41 @@ public class DragonManager : MonoBehaviour
 				Vector3 intersectionPoint = info.point;
 				currentPosition = info.point;
 				float tempDistance = float.PositiveInfinity;
-				if (Input.GetMouseButtonDown(0))
+
+				if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
 				{
-					foreach (GameObject draggable in GameObject.FindGameObjectsWithTag("Draggable"))
+					if (Input.GetMouseButtonDown(0))
 					{
-						float distance = Vector3.Distance(intersectionPoint, draggable.transform.position);
-						if (distance < tempDistance)
+						foreach (GameObject draggable in GameObject.FindGameObjectsWithTag("Draggable"))
 						{
-							tempDistance = distance;
-							dragged = draggable;
+							float distance = Vector3.Distance(intersectionPoint, draggable.transform.position);
+							if (distance < tempDistance)
+							{
+								tempDistance = distance;
+								dragged = draggable;
+							}
 						}
-						lastPosition = currentPosition;
 					}
+					lastPosition = currentPosition;
 				}
 
-				CollisionDetection cd = dragged.GetComponent<CollisionDetection>();
+				Vector3 move = (currentPosition - lastPosition);
+				if (dragged != null)
+				{
+					CollisionDetection cd = dragged.GetComponent<CollisionDetection>();
+					if (cd == null || cd.MayBeMoved)
+						dragged.transform.position += move;
+				}
+				else if (Input.GetMouseButton(1))
+				{
+					move.x = 0;
 
-				if (cd == null || cd.MayBeMoved)
-					dragged.transform.position += (currentPosition - lastPosition);
+					Camera.main.transform.position -= move;
+					currentPosition -= move;
+				}
 
 				lastPosition = currentPosition;
 			}
-
 		}
 	}
 }
