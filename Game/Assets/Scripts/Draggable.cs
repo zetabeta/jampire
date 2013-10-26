94e75d28d0c484427a68d7cb4c1169daf1f85d6a
THplusplus <thomas.hilbert@gameforge.com>
2013-10-26 16:52:11 +0200

    hgj

---
+++ Game/Assets/Scripts/Draggable.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour
+{
+	Vector3 pos;
+	Vector3 lastValidPosition;
+	bool isColliding = false;
+	bool isBeingDragged = false;
+	bool isCastingShadows;
+
+	public bool MayBeMoved { get { return !isColliding; } }
+	public bool IsBeingDragged
+	{
+		get { return isBeingDragged; }
+		set
+		{
+			isBeingDragged = value;
+			collider.enabled = !isBeingDragged;
+		}
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		isCastingShadows = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!isColliding)
+		{
+			lastValidPosition = pos;
+			pos = transform.position;
+		}
+
+		if (isBeingDragged)
+			setCastShadows((((int)(Time.time * 20)) % 2) == 0);
+		else
+			setCastShadows(true);
+	}
+
+	void setCastShadows(bool enabled)
+	{
+		if (isCastingShadows == enabled)
+			return;
+
+		foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+			renderer.castShadows = enabled;
+
+		isCastingShadows = enabled;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		isColliding = true;
+		rigidbody.MovePosition(lastValidPosition);
+		pos = lastValidPosition;
+	}
+
+	void OnCollisionStay()
+	{
+		rigidbody.MovePosition(lastValidPosition);
+		isColliding = false;
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		rigidbody.MovePosition(lastValidPosition);
+		isColliding = false;
+	}
+}
