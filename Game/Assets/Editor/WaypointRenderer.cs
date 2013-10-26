a21e1b00a9ebe1720101c1371a45502bb243f451
THplusplus <thomas.hilbert@gameforge.com>
2013-10-26 11:39:55 +0200

    Waypoints rendern jetzt ebenfalls ihren Pfad

---
+++ Game/Assets/Editor/WaypointRenderer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Waypoint))]
+public sealed class WaypointRenderer : Editor
+{
+	void OnSceneGUI()
+	{
+		Path path = (target as Waypoint).transform.parent.GetComponent<Path>();
+
+		Transform lastChild = null;
+
+		for (int i = 0; i < path.transform.childCount; ++i)
+		{
+			Transform child = path.transform.GetChild(i);
+
+			if (lastChild != null)
+			{
+				Handles.DrawLine(lastChild.position, child.position);
+			}
+
+			lastChild = child;
+		}
+	}
+}
